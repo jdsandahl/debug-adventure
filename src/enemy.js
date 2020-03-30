0d363c512b156055cdaf81f588eb386ddfea5ed6
Justin Sandahl <sandahl.j.d@gmail.com>
2020-03-30 16:42:14 +0100

    fixed most bugs, need to sort out issue with enemy.test not running in Jest

--- src/enemy.js
+++ src/enemy.js
@@ -1,9 +1,9 @@
 /* eslint-disable func-names */
-const Character = require('../character');
+const Character = require('./character');
 
 function Enemy(config) {
   Character.call(this, config);
-  damage = config.damage;
+  this.damage = config.damage;
 }
 
 Enemy.prototype = Object.create(Character.prototype);
@@ -18,8 +18,8 @@ Enemy.prototype._describeAttack = function (target) {
   `${this.name} lets out a ${this.dialogue}, and hits ${target.name} for ${this.damage} damage!`;
 
 
-enemy.prototype.attack = function (target) {
-  target._takeDamage(damage);
+Enemy.prototype.attack = function (target) {
+  target._takeDamage(this.damage);
   return this._describeAttack(target);
 };
 
